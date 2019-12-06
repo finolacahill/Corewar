c5454f0a5013abe6d873bcf1be682148fc64a001
Finola <finola.cahill@gmail.com>
2019-12-06 17:45:08 +0100

    updating process loader

--- main/main.c
+++ main/main.c
@@ -15,14 +15,21 @@
 int main(int ac, char **av)
 {
 	t_all vm;
+	t_process process;
 
 	dasm_init(&vm, ac, av);
 	if (init_arena(&vm) == -1)
-	{
+		{
 		printf("Error initialising arena.\n");
 		dasm_free(&vm);
 		return (0);
 	}
+	if (run_vm(&vm) == -1)
+	{
+		//free the things;
+		return (0);
+	}
 	dasm_free(&vm);
+	//free the vm
 }
 
