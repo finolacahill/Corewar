c0cd1cd6264f77340398049c20f3783e1788c55c
Flavien AFONSO <flafonso@e1r10p20.42.fr>
2019-12-15 15:13:13 +0100

    on ne garde que les fichier clean de du dasm (meme si il reste une erreur avec flag n et le dump a gerer)

--- testouille_2.s
+++
@@ -1,8 +0,0 @@
-.name "TESTOUILLES"
-.comment "sisi 347"
-
-ld		%0, r1
-ld		%58, r2
-aff r1
-aff r2
-zjmp	%45
--- testouille_3.s
+++
@@ -1,8 +0,0 @@
-.name "TESTOUILLES"
-.comment "sisi 347"
-
-ld %2, r1
-st r1, :live
-st r1, r2
-
-live: live %0
