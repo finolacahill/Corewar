edf0a83672cc795be92b8b28c6bfbbaeb51d6db6
Finola CAHILL <fcahill@e2r10p15.42.fr>
2020-01-09 20:35:44 +0100

    added malloc protections

--- src_asm/init.c
+++ src_asm/init.c
@@ -12,12 +12,17 @@
 
 #include "../includes/corewar.h"
 
-t_label		*new_label(void)
+t_label		*new_label(t_env *n)
 {
 	t_label	*new;
 
 	if (!(new = (t_label*)malloc(sizeof(t_label))))
+	{
+		free(n->header);
+		free(n->instruc);
+		free(n);
 		error(8, -1, -1, NULL);
+	}
 	new->label = NULL;
 	new->adress = 0;
 	new->size = 0;
@@ -25,17 +30,21 @@ t_label		*new_label(void)
 	return (new);
 }
 
-t_instruc	*new_instruct(void)
+t_instruc	*new_instruct(t_env *n)
 {
 	t_instruc *new;
 
 	if (!(new = (t_instruc*)malloc(sizeof(t_instruc))))
+	{
+		free(n->header);
+		free(n);
 		error(8, -1, -1, NULL);
+	}
 	new->opcode = 0;
 	new->params = NULL;
 	new->ocp = 0;
 	new->adress = 0;
-	new->label = new_label();
+	new->label = new_label(n);
 	new->is_ocp = 0;
 	new->hexa_params = NULL;
 	new->hexa_instruc = NULL;
@@ -53,16 +62,19 @@ t_env		*init_env(void)
 	if (!(new = (t_env*)malloc(sizeof(t_env))))
 		error(8, -1, -1, NULL);
 	if (!(new->header = (t_header*)malloc(sizeof(t_header))))
+	{
+		free(new);
 		error(8, -1, -1, NULL);
+	}
 	new->header->c = 0;
 	new->header->n = 0;
 	new->header->filename = NULL;
 	new->header->fd = 0;
 	new->header->magic = 0;
 	new->header->prog_size = 0;
-	new->instruc = new_instruct();
+	new->instruc = new_instruct(new);
 	new->line = 0;
 	new->column = 0;
-	new->label = new_label();
+	new->label = new_label(new);
 	return (new);
 }
