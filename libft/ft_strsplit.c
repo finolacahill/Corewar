ab46687b29a982bde3337b69177ea2e56c0f9904
Yohann DANA <yodana@e2r9p11.42.fr>
2020-01-09 16:44:53 +0100

    push new strsplit

--- libft/ft_strsplit.c
+++ libft/ft_strsplit.c
@@ -3,70 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strsplit.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: flafonso <flafonso@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yodana <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/16 14:44:34 by fcahill           #+#    #+#             */
-/*   Updated: 2020/01/09 16:35:21 by flafonso         ###   ########.fr       */
+/*   Created: 2018/11/26 12:50:21 by yodana            #+#    #+#             */
+/*   Updated: 2018/11/26 16:47:07 by yodana           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void			ft_insert(const char *s, char *fresh, int start, int end)
+static int	ft_mot(char const *s, char c)
 {
-	int				j;
+	int i;
+	int words;
 
-	j = 0;
-	while (start != end)
-	{
-		fresh[j] = s[start];
-		j++;
-		start++;
-	}
-	fresh[j] = '\0';
-}
-
-static int			ft_start(const char *s, char c, int start, int end)
-{
-	start = end;
-	while (s[start] == c)
-		++start;
-	return (start);
-}
-
-static int			ft_end(const char *s, char c, int start, int end)
-{
-	end = start;
-	while (s[end] != c && s[end] != '\0')
-		++end;
-	return (end);
-}
-
-char				**ft_strsplit(char const *s, char c)
-{
-	char			**fresh;
-	int				i;
-	int				start;
-	int				end;
-
-	end = 0;
+	words = 0;
 	i = 0;
-	if (!(s) || !(c))
-		return (NULL);
-	if (!(fresh = (char**)malloc(sizeof(char*) * ft_wordcounter(s, c) + 1)))
-		return (NULL);
-	while (i < ft_wordcounter(s, c))
+	while (s[i])
 	{
-		start = ft_start(s, c, start, end);
-		end = ft_end(s, c, start, end);
-		if (s[start] != '\0')
-		{
-			if (!(fresh[i] = (char*)malloc(sizeof(char) * end - start + 1)))
-				return (NULL);
-			ft_insert(s, fresh[i], start, end);
-		}
+		while (s[i] == c && s[i])
+			i++;
+		if (s[i])
+			words++;
+		while (s[i] != c && s[i])
+			i++;
+	}
+	return (words);
+}
+
+char		**ft_strsplit(char const *s, char c)
+{
+	char	**new;
+	int		i;
+	int		j;
+	int		k;
+
+	k = 0;
+	i = 0;
+	j = 0;
+	if (s == NULL || !(new = (char**)malloc(sizeof(char*) * ft_mot(s, c) + 1)))
+		return (NULL);
+	while (i < ft_mot(s, c))
+	{
+		while (s[j] == c && s[j])
+			j++;
+		if (!(new[i] = (char*)malloc(sizeof(char) * j + 1)))
+			return (NULL);
+		while (s[j] != c && s[j])
+			new[i][k++] = (char)s[j++];
+		new[i][k] = '\0';
+		k = 0;
 		i++;
 	}
-	fresh[i] = NULL;
-	return ((char**)fresh);
+	new[i] = NULL;
+	return (new);
 }
