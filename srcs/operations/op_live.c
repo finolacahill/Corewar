8d3b0b92876ec2301515bc9bdc4a5164ad494c56
Finola CAHILL <fcahill@e2r10p15.42.fr>
2020-01-09 18:36:20 +0100

    remove negative id

--- srcs/operations/op_live.c
+++ srcs/operations/op_live.c
@@ -28,7 +28,7 @@ int			is_player_nb(int id, t_all *vm)
 	i = -1;
 	while (++i < vm->total_champ)
 	{
-		if (vm->champs[i].id == -id)
+		if (vm->champs[i].id == id)
 		{
 			vm->champs[i].last_live = vm->cycles;
 			return (i);
